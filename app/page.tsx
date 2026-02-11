48c2d2eeecff330e9e414a5ea164dd0daeb1b781
pathomphon <6740011036@psu.ac.th>
2026-02-11 21:56:30 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,100 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+const Tipcal = () => {
+  const [bill, setBill] = useState(""); 
+  const [tip, setTip] = useState(0); 
+  const [tipAmount, setTipAmount]= useState(0); 
+  const [totalBill, setTotalBill]= useState(0); 
+
+  const calculate = () => {
+      const billNumber = parseFloat(bill) 
+      const tipValue = billNumber * (tip / 100) 
+      const totalValue = billNumber + tipValue 
+      
+      setTipAmount(tipValue) 
+      setTotalBill(totalValue) 
+      
+  }
+
+  return(
+    <div className="w-full min-h-screen justify-center p-6">
+
+      <div className="w-full mx-auto space-y-8"></div>
+
+
+    <div className="w-full bg-white rounded-3xl shadow-xl p-4 mb-8">
+
+      <h1 className="text-4xl font-bold text-center text-slate-800 mb-10">
+        Tip Calculator</h1>
+      </div>
+
+      
+
+        <div className="space-y-6">
+        
+        <div className="w-full mb-10">  
+
+          <label className="block text-lg font-semibold text-slate-600 mb-2">Bill Amount
+          </label>
+          <input type="number"
+
+          value={bill}
+
+             onChange={(x) => setBill(x.target.value)} //  onchange ทำงานเมื่อ input เปลี่ยน / e คือตัวแปรเหตุการณ์ ,target ตัวที่ถูกพิมพ์, value ค่าปัจจุบันในช่อง
+
+          placeholder="Enter Bill Amount"
+          
+          className="text-xl lg:w-full w-full rounded-xl border border-slate-300 p-4 focus:outline-none focus:ring-2 focus:ring-indigo-400"    
+
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </div>
+
+      <div>
+        <p className="text-lg font-semibold text-slate-600 mb-4">Select Tip %
+        </p>
+
+        <div className="grid grid-cols-1 gap-4 mb-4">
+ 
+          <button onClick={() => setTip(tip === 5 ? 0 : 5)} //  ถ้า tip เท่ากับ 5  ใช้ค่า 0 ถ้า tip ไม่เท่ากับ 5  ใช้ค่า 5
+
+          className={`rounded-2xl bg-indigo-100 hover:bg-indigo-300 hover:text-white transition text-lg p-4 font-semibold 
+            ${tip === 5 ? "bg-indigo-300 text-white" : "bg-indigo-100"}`}>
+              5%
+              </button>
         </div>
-      </main>
+      </div>
+
+     <button onClick={calculate}
+
+     className="text-2xl text-white font-bold bg-indigo-500 hover:bg-indigo-600 w-full rounded-2xl py-4 transition"
+
+     >Calculate
+     </button>
+      
+      <div className="space-y-8">
+
+      <div className="rounded-2xl bg-slate-50 p-6">
+
+       <p className="text-lg font-semibold text-slate-500 mb-2">
+        Tip Total</p>
+
+       <p className="text-4xl font-bold text-slate-500 mb-2">฿{tipAmount}</p>
+
+       </div>
     </div>
-  );
+
+      <div className="rounded-2xl p-6 bg-indigo-50">
+
+       <p className=" text-lg font-semibold text-indigo-500 mb-2 ">Bill Total</p>
+
+       <p className="text-5xl font-bold text-indigo-600">฿
+        {totalBill}</p>
+
+        </div>
+    </div>
+  )
+
 }
+export default Tipcal;
